6f38e1c45160d4522aa9d7ad8cedd4a8b0bc63cc
Dell <Dell@DESKTOP-UBQU0Q8>
2022-06-13 23:52:08 -0500

    Imprimir variable

--- src/EjemploGit.java
+++ src/EjemploGit.java
@@ -3,6 +3,10 @@ public class EjemploGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hola desde eclipse...");
+		
+		String nombre = "Martin Sabnango";
+		
+		System.out.println();
 
 	}
 
